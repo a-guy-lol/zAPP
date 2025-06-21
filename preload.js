c12611cf1cb5aaba47cf326243c774fdbb7ce172
A guy lol <meisguy.x9@gmail.com>
2025-06-21 01:39:19 -0400

    a

--- preload.js
+++ preload.js
@@ -3,16 +3,13 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   minimizeWindow: () => ipcRenderer.send('minimize-window'),
   closeWindow: () => ipcRenderer.send('close-window'),
-  
   openSignupLink: () => ipcRenderer.send('open-signup-link'),
   login: (credentials) => ipcRenderer.invoke('login', credentials),
-
   checkConnection: () => ipcRenderer.invoke('check-connection'),
   executeScript: (scriptContent) => ipcRenderer.invoke('execute-script', scriptContent),
-  
   loadState: () => ipcRenderer.invoke('load-state'),
   saveState: (data) => ipcRenderer.invoke('save-state', data),
-
   onRequestFinalSave: (callback) => ipcRenderer.on('request-final-save', callback),
   notifyFinalSaveComplete: () => ipcRenderer.send('final-save-complete'),
+  updateActiveScriptName: (scriptName) => ipcRenderer.send('update-active-script-name', scriptName),
 });
